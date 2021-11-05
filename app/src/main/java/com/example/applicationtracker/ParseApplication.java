36a2e612b731a53219580c701087c606e8e7c921
RodrigoMariaca1995 <rlmariac@cougarnet.uh.edu>
2021-11-05 15:12:29 -0500

    Created login, logout, and register page. Created backend database

---
+++ app/src/main/java/com/example/applicationtracker/ParseApplication.java
@@ -0,0 +1,24 @@
+package com.example.applicationtracker;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        //ParseObject.registerSubclass(Application.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("qyNnfVMVIkKLp9uEmKCZ5NTZPzGM5YqtatQSd0F2")
+                .clientKey("E0uVfyjtvvPVHkrBglBbFFIspF4W82mr41NvdnGn")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
