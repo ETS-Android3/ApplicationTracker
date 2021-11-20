1db7938db02fa179cc85d1887c810074abac5544
AnilShanker <the235anil@hotmail.com>
2021-11-20 16:38:31 -0600

    Implemented delete application button inside of DetailActivity

--- app/src/main/java/com/example/applicationtracker/DetailActivity.java
+++ app/src/main/java/com/example/applicationtracker/DetailActivity.java
@@ -1,26 +1,23 @@
 package com.example.applicationtracker;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.example.applicationtracker.adapters.ApplicationsAdapter;
 import com.example.applicationtracker.models.Application;
+import com.parse.DeleteCallback;
+import com.parse.ParseException;
 
 import org.parceler.Parcels;
 
-import java.util.ArrayList;
-import java.util.Date;
-
 public class DetailActivity extends AppCompatActivity {
+
+    public static final String TAG = "DetailActivity";
+
     TextView tvCompanyName;
     TextView tvDateApplied;
     TextView tvStatus;
@@ -28,6 +25,7 @@ public class DetailActivity extends AppCompatActivity {
     TextView tvJobTitle;
     Application app;
     Button btnEditActivity;
+    Button btnDeleteApp;
 
 
     @Override
@@ -43,12 +41,30 @@ public class DetailActivity extends AppCompatActivity {
         tvNotes = findViewById(R.id.tvNotes);
         tvJobTitle = findViewById(R.id.jobTitle);
         btnEditActivity = findViewById(R.id.btnEditActivity);
+        btnDeleteApp = findViewById(R.id.btnDeleteApp);
 
         btnEditActivity.setOnClickListener(view -> {
             Intent i = new Intent(this, EditActivity.class);
             i.putExtra(Application.class.getSimpleName(), Parcels.wrap(app));
             startActivity(i);
-            Log.i("DetailActivity", "clicked edit button");
+            Log.i(TAG, "clicked edit button");
+        });
+
+        // Deletes currently opened application from Parse
+        btnDeleteApp.setOnClickListener(view -> {
+            app.deleteInBackground(new DeleteCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e != null) {
+                        Log.e(TAG, "done: ", e);
+                    } else {
+                        Intent i = new Intent(DetailActivity.this, MainActivity.class);
+                        startActivity(i);
+                        Log.i(TAG, "done: Deleted application");
+                        finish();
+                    }
+                }
+            });
         });
 
 
